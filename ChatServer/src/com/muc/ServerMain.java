fbed3048d2a329d1303ea6e07589a83a2f244838
BenM88 <benmadarieta@gmail.com>
2019-04-29 12:40:45 -0600

    Concurrency Working

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -15,21 +15,33 @@ public class ServerMain {
                 System.out.println("About to accept client connection...");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from" +clientSocket);
-                OutputStream outputStream = clientSocket.getOutputStream();
-                for(int i=0; i<10; i++){
-                    outputStream.write(("time now is" + new Date() + "\n").getBytes());
-                    Thread.sleep(1000);
-
-                }
-                outputStream.write("Hello World its ben\n".getBytes());
-                clientSocket.close();
+                Thread t = new Thread() {
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
             }
 
         } catch (IOException e) {
             e.printStackTrace();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
         }
+    }
 
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for(int i=0; i<10; i++){
+            outputStream.write(("time now is" + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+
+        }
+        outputStream.write("Hello World its ben\n".getBytes());
+        clientSocket.close();
     }
 }
