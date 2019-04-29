1b700c10a0f631531443cd3c5849f6a9b8137d05
BenM88 <benmadarieta@gmail.com>
2019-04-29 13:06:28 -0600

    Refactored verison after recurrency

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -15,19 +15,9 @@ public class ServerMain {
                 System.out.println("About to accept client connection...");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from" +clientSocket);
-                Thread t = new Thread() {
-                    @Override
-                    public void run() {
-                        try {
-                            handleClientSocket(clientSocket);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                t.start();
+                ServerWorker worker = new ServerWorker(clientSocket);
+                worker.start();
+
             }
 
         } catch (IOException e) {
@@ -35,14 +25,5 @@ public class ServerMain {
         }
     }
 
-    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
-        OutputStream outputStream = clientSocket.getOutputStream();
-        for(int i=0; i<10; i++){
-            outputStream.write(("time now is" + new Date() + "\n").getBytes());
-            Thread.sleep(1000);
 
-        }
-
-        clientSocket.close();
-    }
 }
